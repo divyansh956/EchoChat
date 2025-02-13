ecac75c6029bf893e7adecc92e2e0b750f019c7a
Divyansh Saxena <divyanshsxn998@gmail.com>
2025-02-13 19:12:50 +0530

    route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,37 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import SignUpPage from "./pages/SignUpPage";
+
 const App = () => {
-  return <div className="bg-blue-500">App</div>;
+  const authUser = false;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
