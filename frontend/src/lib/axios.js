5cf58185c4ea2cefa62a290282a1c289fc2bd3c6
Divyansh Saxena <divyanshsxn998@gmail.com>
2025-02-14 11:21:10 +0530

    Profile Page

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:1337/api"
-      : "/api",
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
 });
